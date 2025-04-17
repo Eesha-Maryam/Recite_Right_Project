d211fb499e77e0d82b26d1911f849eeda7b8c116
Esha Maryam <eshamaryam254@gmail.com>
2025-04-17 12:23:05 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'login.dart';
 
 void main() {
@@ -12,6 +12,36 @@ class MyApp extends StatelessWidget {
       // Removed 'const' here
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+    );
+  }
+}
+
+*/
+
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'quran.dart';
+import 'mutashabihat.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recite Right',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        '/quran': (context) => const QuranPage(),
+        '/mutashabihat': (context) => const MutashabihatPage(),
+      },
     );
   }
 }
